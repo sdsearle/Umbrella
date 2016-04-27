37494eb3828b32e66d7c54bb3707bf9ffbe2a895
Andrew Reitz <aj.reitz@gmail.com>
2016-04-27 08:37:15 -0500

    Updated Project
    - Updated project dependencies and gradle file
    - Removed GridLayoutManager work around

---
+++ Umbrella/src/main/java/com/nerdery/umbrella/data/api/WeatherService.java
@@ -0,0 +1,27 @@
+package com.nerdery.umbrella.data.api;
+
+import com.nerdery.umbrella.BuildConfig;
+import com.nerdery.umbrella.data.model.WeatherData;
+import retrofit2.Call;
+import retrofit2.adapter.rxjava.Result;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+/**
+ * Retrofit interface for fetching weather data
+ */
+public interface WeatherService {
+
+    /**
+     * Get the forecast for a given zip code using {@link Call}
+     */
+    @GET("/api/" + BuildConfig.API_KEY + "/conditions/hourly/q/{zip}.json")
+    Call<WeatherData> forecastForZipCallable(@Path("zip") String zipCode);
+
+    /**
+     * Get the forecast for a given zip code using {@link Observable}
+     */
+    @GET("/api/" + BuildConfig.API_KEY + "/conditions/hourly/q/{zip}.json")
+    Observable<Result<WeatherData>> forecastForZipObservable(@Path("zip") String zipCode);
+}
