4de64af2e1d9d2bb476d3517bcfbd7960f6c37e9
Kenton Watson <kwatson@nerdery.com>
2018-01-09 18:13:29 -0600

    Update retrofit call factory.

--- Umbrella/src/main/java/com/nerdery/umbrella/data/api/WeatherService.java
+++ Umbrella/src/main/java/com/nerdery/umbrella/data/api/WeatherService.java
@@ -5,7 +5,7 @@ import com.nerdery.umbrella.data.model.WeatherData;
 
 import io.reactivex.Single;
 import retrofit2.Call;
-import retrofit2.adapter.rxjava.Result;
+import retrofit2.adapter.rxjava2.Result;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import rx.Observable;
