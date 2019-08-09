9e32e9198106c404a4040403d912a00af810859f
Mike Wolfson <mwolfson@gmail.com>
2019-08-09 14:32:37 -0700

    BRAVOANDROID-210 Update to Jetpack
    This also updates the rest of the dependencies in the project

--- Umbrella/src/main/java/com/nerdery/umbrella/ui/MainActivity.kt
+++ Umbrella/src/main/java/com/nerdery/umbrella/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.nerdery.umbrella.ui
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.nerdery.umbrella.R
 
 class MainActivity : AppCompatActivity(){
