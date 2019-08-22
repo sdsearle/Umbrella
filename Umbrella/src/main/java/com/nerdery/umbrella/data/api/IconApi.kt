b0a5f01d2d4fea2cd942bfe1ca0927fa846a79f8
Mike Wolfson <mwolfson@gmail.com>
2019-08-22 10:37:27 -0700

    This had a ref to Weather Underground
    This had a nad reference

--- Umbrella/src/main/java/com/nerdery/umbrella/data/api/IconApi.kt
+++ Umbrella/src/main/java/com/nerdery/umbrella/data/api/IconApi.kt
@@ -9,7 +9,7 @@ class IconApi {
 
     /**
      * Get the URL to an icon suitable for use as a replacement for the icons given by Weather Underground
-     * @param icon The name of the icon provided by Weather Underground (e.g. "clear").
+     * @param icon The name of the icon provided by the DarkSky API (e.g. "clear").
      * @param highlighted True to get the highlighted version, false to get the outline version
      * @return A URL to an icon
      */
